1f987e6d9dfd50830bed5d6347e37ce97552796d
Mishelle23 <michaela.med@outlook.com>
2021-08-06 01:07:52 +0100

    Endpoints

--- index.js
+++ index.js
@@ -1,78 +1,169 @@
 const express = require("express");
+
 morgan = require("morgan");
+uuid = require("uuid");
+bodyParser = require("body-parser");
 
 const app = express();
 
-let topTen = [
-  //1
-  {
-    title: "The Age of Adaline"
-  },
+app.use(morgan("common"));
 
-  //2
-  {
-    title: "The Impossible"
-  },
+app.use(bodyParser.json());
 
-  //3
-  {
-    title: "Sweet November"
-  },
+app.use(express.static("public"));
 
-  //4
+// JSON Object 10 Top Movies
+let movies = [
   {
-    title: "Sully"
+    title: "The Age of Adaline",
+    actor: "Blake Lively",
+    director: "Lee Toland Krieger",
+    genre: "romance"
   },
-
-  //5
   {
-    title: "Into the Wild"
+    title: "The Impossible",
+    actor: "Naomi Watts",
+    director: "J A Bayona",
+    genre: "true story"
   },
-
-  //6
   {
-    title: "The Grey"
+    title: "Sweet November",
+    actor: "Charlize Theron",
+    director: "Pat OConnor",
+    genre: "romance"
   },
-
-  //7
   {
-    title: "Arctic"
+    title: "Sully",
+    actor: "Tom Hanks",
+    director: "Clint Eastwood",
+    genre: "true story"
   },
-
-  //8
   {
-    title: "Jungle"
+    title: "Into the Wild",
+    actor: "Emile Hirsch",
+    director: "Sean Penn",
+    genre: "true story"
   },
-
-  //9
   {
-    title: "Little Women"
+    title: "The Grey",
+    actor: "Liam Neeson",
+    director: "Joe Carnahan",
+    genre: "thriler"
   },
-
-  //10
   {
-    title: "Patch Adams"
+    title: "Arctic",
+    actor: "Mads Mikkelsen",
+    director: "Joe Penna",
+    genre: "true story"
+  },
+  {
+    title: "Jungle",
+    actor: "Daniel Radcliffe",
+    director: "Greg McLean",
+    genre: "true story"
+  },
+  {
+    title: "Little Women",
+    actor: "Florence Pugh",
+    director: "Greta Gerwig",
+    genre: "classic"
+  },
+  {
+    title: "Patch Adams",
+    actor: "Robin Williams",
+    director: "Tom Shadyac",
+    genre: "true story"
   }
 ];
 
-app.use(morgan("common"));
-app.use(express.static("public"));
-
 // GET requests
 app.get("/", (req, res) => {
-  res.send("My movie API");
+  res.send("Let Me Present My First Movie API");
 });
 
+// Returning a list of ALL movies to the user
 app.get("/movies", (req, res) => {
-  res.json(topTen);
+  res.json(movies);
+});
+
+// Returning data (description, genre, director) about a single movie
+app.get("/movies/:title", (req, res) => {
+  res.json(
+    movies.find(movie => {
+      return movie.title === req.params.title;
+    })
+  );
+});
+
+// Return data about a genre
+app.get("/genres/:title", (req, res) => {
+  res.json(
+    movies.find(genre => {
+      return genre.title === req.params.title;
+    })
+  );
+});
+
+// Returning data about a director
+app.get("/directors/:director", (req, res) => {
+  res.json(
+    movies.find(director => {
+      return director.director === req.params.director;
+    })
+  );
+});
+
+app.get("/actors/:title", (req, res) => {
+  res.json(
+    movies.find(actor => {
+      return actor.title === req.params.title;
+    })
+  );
+});
+
+// Allowing new users to register
+app.post("/users", (req, res) => {
+  let newUser = req.body;
+
+  if (!newUser.name) {
+    const message = "Missing name in request body";
+    res.status(400).send(message);
+  } else {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).send(newUser);
+  }
+});
+
+// Allowing the user to update their user info (username)
+app.put("/users/:username", (req, res) => {
+  let userName = users.find(username => res.send("User was not found."));
+});
+
+// Allowing the user to add a movie to the favourites list
+app.put("/users/movies/:favorites", (req, res) => {
+  let userFavorite = movies.find(movie => res.send("Movie added to favorites"));
+});
+
+// Allowing the user to remove a movie from the favorites list
+app.delete("/users/movies/:favorites", (req, res) => {
+  let userFavorite = movies.find(movie =>
+    res.send("Movie has been deleted from favorites")
+  );
+});
+
+// Allowing already existing user to unregister
+app.delete("/users/:username", (req, res) => {
+  let deRegister = users.find(user =>
+    res.send("user / details have been removed")
+  );
 });
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send("Something Broke!");
+  res.status(500).send("Something broke!");
 });
-
-// listen for requests
+// Listen for requests
 app.listen(8080, () => {
   console.log("Your app is listening on port 8080.");
 });
