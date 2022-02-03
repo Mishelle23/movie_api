406cc929a08061b85b295c7d28ad5aa7bd13566d
Mishelle23 <michaela.med@outlook.com>
2022-02-03 19:42:57 +0000

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,19 @@ require("./passport");
 
 auth = require("./auth")(app);
 const cors = require("cors");
-app.use(cors());
+/*app.use(cors());*/
+let allowedOrigins = ['http://localhost:1234', 'http://testsite.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn't found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn't allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // GET requests
 app.get("/", (req, res) => {
